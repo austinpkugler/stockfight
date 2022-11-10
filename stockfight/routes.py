ae6a1a30814ed0da4b42c3eb28a077543dc5e4c5
austinpkugler <austinpkugler@gmail.com>
2022-11-10 15:18:57 -0800

    Solidify page styles

--- stockfight/routes.py
+++ stockfight/routes.py
@@ -6,3 +6,18 @@ from stockfight import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/account/')
+def account():
+    return render_template('account.html')
+
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/leaderboard/')
+def leaderboard():
+    return render_template('leaderboard.html')
